86e19c83d83e1a54d5757db08ae942660a1e2731
Vinícius Vilas Boas <vilasboas503@gmail.com>
2025-08-20 12:18:26 -0300

    Added NotFound and About page
    Also added GenericHtml

--- src/templates/MainTemplate/index.tsx
+++ src/templates/MainTemplate/index.tsx
@@ -1,17 +1,15 @@
 import { Container } from '../../components/Container';
-import { Countdown } from '../../components/Countdown';
 import { Footer } from '../../components/Footer';
 import { Logo } from '../../components/Logo';
-import { MainForm } from '../../components/MainForm';
 import { Menu } from '../../components/Menu';
-import './styles/global.css';
-import './styles/theme.css';
+import '../../styles/global.css';
+import '../../styles/theme.css';
 
 type MainTemplateProps = {
     children: React.ReactNode;
 };
 
-function MainTemplate({ children }: MainTemplateProps) {
+export function MainTemplate({ children }: MainTemplateProps) {
     return (
         <>
             <Container>
